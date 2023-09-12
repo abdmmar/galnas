2531fb8fb762dc715312fa82f1800e262cf45805
abdmmar <opowae909@gmail.com>
2023-09-12 12:01:06 +0700

    feat: init search collection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,64 @@
-import { MagnifyingGlassIcon } from '@radix-ui/react-icons'
+import { Search } from '@/app/search'
+import galnas from '../data/galeri-nasional.json'
 
-export default function Home() {
+type Collection = {
+  title: string
+  image?: string
+  artist: {
+    name: string
+    link: string
+  }
+  year: number
+  medium: string
+  description: string
+  link: string
+  size?: string
+}
+
+type Collections = {
+  total: number
+  paintings: {
+    total: number
+    data: Array<Collection>
+  }
+  sculptures: {
+    total: number
+    data: Array<Collection>
+  }
+  others: {
+    total: number
+    data: Array<Collection>
+  }
+}
+
+const items: Array<Collection> = galnas.paintings.data.concat(
+  galnas.sculptures.data,
+  galnas.others.data,
+)
+
+const createColumns = (data: Array<Collection>) => {
+  let col = 0
+  const columns: Array<Array<Collection>> = [[], [], [], []]
+  for (let i = 0; i < data.length; i++) {
+    const collection = data[i]
+    if (col > 3) col = 0
+    columns[col].push(collection)
+    col++
+  }
+  return columns
+}
+
+export default async function Home({ searchParams }: { searchParams: { title: string } }) {
+  const collections = items.filter((item) => item.title.toLowerCase().includes(searchParams.title))
+  const columns = createColumns(collections)
+
   return (
     <div className="flex w-full gap-10">
       <div className="flex w-1/4">
         <ul className="w-full">
-          <li>image</li>
+          {columns[0].map((c) => (
+            <li>{c.title}</li>
+          ))}
         </ul>
       </div>
       <div className="flex w-1/2">
@@ -14,7 +67,7 @@ export default function Home() {
             <header className="flex items-center justify-between">
               <div>Dokumentasi</div>
               <div>
-                <MagnifyingGlassIcon />
+                <Search />
               </div>
             </header>
             <main>
@@ -29,17 +82,23 @@ export default function Home() {
           </div>
           <div className="flex w-full flex-row gap-10">
             <ul className="w-full">
-              <li>image</li>
+              {columns[1].map((c) => (
+                <li>{c.title}</li>
+              ))}
             </ul>
             <ul className="w-full">
-              <li>image</li>
+              {columns[2].map((c) => (
+                <li>{c.title}</li>
+              ))}
             </ul>
           </div>
         </div>
       </div>
       <div className="flex w-1/4">
         <ul className="w-full">
-          <li>image</li>
+          {columns[3].map((c) => (
+            <li>{c.title}</li>
+          ))}
         </ul>
       </div>
     </div>
---
+++ src/app/search.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import { Input } from '@/components/ui/input'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import { useCallback } from 'react'
+
+export function Search() {
+  const router = useRouter()
+  const pathname = usePathname()
+  const searchParams = useSearchParams()!
+
+  const createQueryString = useCallback(
+    (name: string, value: string) => {
+      const params = new URLSearchParams(searchParams)
+      params.set(name, value)
+
+      return params.toString()
+    },
+    [searchParams],
+  )
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target
+    router.push(pathname + '?' + createQueryString('title', value))
+  }
+
+  return <Input onChange={onChange} />
+}
