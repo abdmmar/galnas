7d23b076cd20d94d82fba1f15b0f3ae5f50aa5a1
abdmmar <opowae909@gmail.com>
2023-09-13 22:17:23 +0700

    refactor: search

--- src/app/_components/search.tsx
+++ src/app/_components/search.tsx
@@ -32,14 +32,8 @@ type Sort = '' | 'title:asc' | 'title:desc' | 'year:asc' | 'year:desc'
 export function Search() {
   const router = useRouter()
   const pathname = usePathname()
-  const searchParameters = useSearchParams()!
+  const searchParameters = useSearchParams()
 
-  const [filter, setFilter] = React.useState<
-    Record<'medium' | 'classification', Record<string, Checked>>
-  >({ classification: {}, medium: {} })
-  const isFiltered =
-    Object.values(filter.classification).includes(true) ||
-    Object.values(filter.medium).includes(true)
   const [sort, setSort] = React.useState<Sort>('')
   const isSorted = sort !== ''
 
@@ -55,67 +49,20 @@ export function Search() {
 
   const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = event.target
-    router.push(pathname + '?' + createQueryString('title', value))
-  }
 
-  const onFilter = (filter: 'medium' | 'classification', key: string, value: boolean) => {
-    setFilter((f) => ({
-      ...f,
-      [`${filter}`]: { ...f[`${filter}`], [`${key}`]: value },
-    }))
+    if (!value) {
+      router.push(pathname)
+      return
+    }
+
+    router.push(pathname + '?' + createQueryString('title', value))
   }
 
   const onSort = (s: Sort) => setSort(s)
 
   return (
     <div className="flex items-center gap-3">
-      <DropdownMenu>
-        <div className="relative">
-          <DropdownMenuTrigger asChild>
-            <Button size="icon" variant="outline">
-              <MixerVerticalIcon className="h-4 w-4" />
-            </Button>
-          </DropdownMenuTrigger>
-          <AnimatePresence>
-            {isFiltered && (
-              <ResetButton
-                onClick={() => setFilter({ classification: {}, medium: {} })}
-                tooltip="Reset Filter"
-              />
-            )}
-          </AnimatePresence>
-        </div>
-        <DropdownMenuContent className="w-56">
-          <DropdownMenuLabel>Classification</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuCheckboxItem
-            checked={filter.classification?.['paintings']}
-            onCheckedChange={(c) => onFilter('classification', 'paintings', c)}
-          >
-            Paintings
-          </DropdownMenuCheckboxItem>
-          <DropdownMenuCheckboxItem
-            checked={filter.classification?.['sculpture']}
-            onCheckedChange={(c) => onFilter('classification', 'sculpture', c)}
-          >
-            Sculpture
-          </DropdownMenuCheckboxItem>
-          <DropdownMenuLabel>Medium</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuCheckboxItem
-            checked={filter.medium?.['Cat minyak pada kanvas.']}
-            onCheckedChange={(c) => onFilter('medium', 'Cat minyak pada kanvas.', c)}
-          >
-            Cat minyak pada kanvas.
-          </DropdownMenuCheckboxItem>
-          <DropdownMenuCheckboxItem
-            checked={filter.medium?.['Akrilik pada kertas.']}
-            onCheckedChange={(c) => onFilter('medium', 'Akrilik pada kertas.', c)}
-          >
-            Akrilik pada kertas.
-          </DropdownMenuCheckboxItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
+      <SearchFilter />
       <Select onValueChange={(v) => onSort(v as Sort)} value={sort}>
         <div className="relative">
           <SelectTrigger className="w-[150px]">
@@ -148,5 +95,90 @@ export function Search() {
         </div>
       </div>
     </div>
+  )
+}
+
+type FilterType = 'medium' | 'classification'
+
+/**
+ * Filter collections.
+ * ?classification=paintings,sculpture&medium=1,2,3,4,5,6&sort=title-asc&search=kapal
+ */
+function SearchFilter() {
+  // const searchParams = useSearchParams()
+  const [filter, setFilter] = React.useState<Record<FilterType, Record<string, Checked>>>({
+    classification: {},
+    medium: {},
+  })
+  const isFiltered =
+    Object.values(filter.classification).includes(true) ||
+    Object.values(filter.medium).includes(true)
+
+  // const createQueryString = React.useCallback(
+  //   (name: string, value: string) => {
+  //     const parameters = new URLSearchParams(searchParams)
+  //     parameters.set(name, value)
+
+  //     return parameters.toString()
+  //   },
+  //   [searchParams],
+  // )
+
+  const onFilter = (type: FilterType, key: string, value: boolean) => {
+    const f = {
+      ...filter,
+      [`${type}`]: { ...filter[`${type}`], [`${key}`]: value },
+    }
+    setFilter(f)
+  }
+
+  return (
+    <DropdownMenu>
+      <div className="relative">
+        <DropdownMenuTrigger asChild>
+          <Button size="icon" variant="outline">
+            <MixerVerticalIcon className="h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <AnimatePresence>
+          {isFiltered && (
+            <ResetButton
+              onClick={() => setFilter({ classification: {}, medium: {} })}
+              tooltip="Reset Filter"
+            />
+          )}
+        </AnimatePresence>
+      </div>
+      <DropdownMenuContent className="w-56">
+        <DropdownMenuLabel>Classification</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuCheckboxItem
+          checked={filter.classification?.['paintings']}
+          onCheckedChange={(c) => onFilter('classification', 'paintings', c)}
+        >
+          Paintings
+        </DropdownMenuCheckboxItem>
+        <DropdownMenuCheckboxItem
+          checked={filter.classification?.['sculpture']}
+          onCheckedChange={(c) => onFilter('classification', 'sculpture', c)}
+        >
+          Sculpture
+        </DropdownMenuCheckboxItem>
+        <DropdownMenuLabel>Medium</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuCheckboxItem
+          checked={filter.medium?.['Cat minyak pada kanvas.']}
+          onCheckedChange={(c) => onFilter('medium', 'Cat minyak pada kanvas.', c)}
+        >
+          Cat minyak pada kanvas.
+        </DropdownMenuCheckboxItem>
+        <DropdownMenuCheckboxItem
+          checked={filter.medium?.['Akrilik pada kertas.']}
+          onCheckedChange={(c) => onFilter('medium', 'Akrilik pada kertas.', c)}
+        >
+          Akrilik pada kertas.
+        </DropdownMenuCheckboxItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
