498f7c1f6f53f2f30fd0f226c6626b5b44cf3d59
abdmmar <opowae909@gmail.com>
2023-09-21 00:04:40 +0700

    refactor: extract collection schema

---
+++ src/app/_schemas/collection.ts
@@ -0,0 +1,33 @@
+import * as z from 'zod'
+
+const artistSchema = z.object(
+  {
+    name: z
+      .string({ required_error: 'Artist name is required' })
+      .nonempty({ message: "Artist name can't be empty" }),
+    link: z.string().optional(),
+  },
+  { required_error: 'Artist is required' },
+)
+
+export const collectionSchema = z.object({
+  title: z
+    .string({ required_error: 'Title is required' })
+    .nonempty({ message: "Title can't be empty" }),
+  description: z
+    .string({ required_error: 'Description is required' })
+    .nonempty({ message: "Description can't be empty" }),
+  classification: z
+    .string({ required_error: 'Classification is required' })
+    .nonempty({ message: "Classification can't be empty" }),
+  year: z
+    .string({ required_error: 'Year is required' })
+    .nonempty({ message: "Year can't be empty" }),
+  medium: z
+    .string({ required_error: 'Medium is required' })
+    .nonempty({ message: "Medium can't be empty" }),
+  artist: artistSchema,
+  image: z.string().optional(),
+  link: z.string().optional(),
+  size: z.string().optional(),
+})
