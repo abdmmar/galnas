6beb6980dd056e82ed4e6f8c2cea5e9cfc6cb952
abdmmar <opowae909@gmail.com>
2023-09-22 00:01:46 +0700

    feat: add collection filter

--- src/app/api/collection/route.tsx
+++ src/app/api/collection/route.tsx
@@ -6,29 +6,80 @@ import { Collection } from '@/app/_types/collection'
 
 export async function GET(request: Request) {
   const url = new URL(request.url)
-  const titleParams = url.searchParams.get('title')
-  const classificationParams = url.searchParams.get('classification')
-  const mediumParams = url.searchParams.get('medium')
   const sortParams = url.searchParams.get('sort')
   const sortValue = sortParams ? sortParams.split(':') : undefined
+  const classificationParams = url.searchParams.get('classification')
+  const mediumParams = url.searchParams.get('medium')
   const params = {
-    title: titleParams,
-    classification: classificationParams?.split(',') || undefined,
-    medium: mediumParams?.split(',') || undefined,
+    title: url.searchParams.get('title'),
+    classification: classificationParams?.split(','),
+    medium: mediumParams?.split(','),
     sort: sortValue && {
       field: sortValue[0],
       value: sortValue[1],
     },
   }
 
-  console.log(params)
-
   const client = new Client(process.env.DATABASE_URL)
 
   try {
     await client.connect()
 
-    const result = await client.query('SELECT * FROM collection')
+    let paramQuery = 1
+    const conditions = []
+    const values = []
+
+    if (params.title) {
+      conditions.push(`c.title ILIKE $${paramQuery}`)
+      values.push(`%${params.title}%`)
+      paramQuery++
+    }
+
+    if (params.classification) {
+      conditions.push(`c.classification IN ($${paramQuery})`)
+      values.push(classificationParams)
+      paramQuery++
+    }
+
+    if (params.medium) {
+      conditions.push(`m.id IN ($${paramQuery})`)
+      values.push(mediumParams)
+      paramQuery++
+    }
+
+    const whereClause = conditions.length > 0 ? `WHERE ${conditions.join(' AND ')}` : ''
+    let orderByClause = ''
+
+    if (params.sort) {
+      orderByClause = `ORDER BY $${paramQuery} ${params.sort.value === 'asc' ? 'ASC' : 'DESC'}`
+      values.push(params.sort.field)
+      paramQuery++
+    }
+
+    const query = `
+    SELECT 
+      c.id AS id,
+      c.title as title,
+      c.description as description,
+      c.year as year,
+      c.size as size,
+      c.classification as classification,
+      c.image as image,
+      c.link as link,
+      JSONB_AGG(JSONB_BUILD_OBJECT('id', m.id, 'name', m.name)) AS medium
+    FROM 
+      collection AS c 
+    JOIN 
+      collection_medium AS cm ON c.id = cm.collection_id 
+    JOIN 
+      medium AS m ON cm.medium_id = m.id
+    ${whereClause}
+    GROUP BY 
+      c.id
+    ${orderByClause}
+    `
+
+    const result = await client.query(query, values)
     const totalRows = result.rowCount
     const collections = result.rows
 
